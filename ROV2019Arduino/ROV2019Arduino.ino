8b04dea8c63b47190141363afccd47a78383207d
Abdulaziz-IT <azzoz_4_ever@hotmail.com>
2019-03-18 08:25:06 +0300

    Adding 2 more moves + renaming variables

--- ROV2019Arduino/ROV2019Arduino.ino
+++ ROV2019Arduino/ROV2019Arduino.ino
@@ -3,12 +3,12 @@
 //On standard servos a parameter value of 1000 is fully counter-clockwise, 2000 is fully clockwiseو  and 1500 is in the middle. (https://www.arduino.cc/en/Reference/ServoWriteMicroseconds)
 
 //Servos definitions
-Servo backLeft;
+Servo servo2;
 Servo backRight;
-Servo midLeft;
-Servo midRight;
-Servo frontLeft;
-Servo frontRight;
+Servo servo4;
+Servo servo3;
+Servo servo6;
+Servo servo5;
 
 //Commands to be read
 String command;
@@ -27,16 +27,42 @@ front left 	= 	6
 front right = 	4
 */
 
+/* Moves:
+Up: 3+4 clockwise
+Down: 3+4 counter-clockwise
+Front: 1+2+5+6 clockwise
+Back: 1+2+5+6 counter-clockwise
+Rotate right: 2+6 clockwise, 1+5 Counter-clockwise
+Rotate left: 1+5 clockwise, 2+6 Counter-clockwise
+Right: 1+6 clockwise, 2+5 counter-clockwise
+Left: 5+2 clockwise, 1+6 counter-clockwise
+Vertical Right = 4 clockwise, 3 counter-clockwise
+Vertical Left = 3 clockwise, 4 counter-clockwise
+
+Combinations:
+Front+up
+Front+down
+
+Back+up
+Back+down
+
+Right+up
+Right+down
+
+Left+up
+Left+down
+*/
+
 void setup() {
 	Serial.begin(9600);
 	
 	//Attaching pins to servos
-	backLeft.attach(3);
-	backRight.attach(2);
-	midLeft.attach(7);
-	midRight.attach(5);
-	frontLeft.attach(6);
-	frontRight.attach(4);	
+	servo2.attach(3); //Back-left servo
+	servo1.attach(2); //Back-right servo
+	servo4.attach(7); //Mid-left servo
+	servo3.attach(5); //Mid-right servo
+	servo6.attach(6); //Front-left servo
+	servo5.attach(4); //Front-right servo
 		
 }
 
@@ -55,12 +81,12 @@ void loop() {
 		moveBack();
 		moving = true;
 		state = command;
-	} else if (command == "left" && !moving) {
-		moveLeft();
+	} else if (command == "rotate left" && !moving) {
+		rotateLeft();
 		moving = true;
 		state = command;
-	} else if (command == "right" && !moving) {
-		moveRight();
+	} else if (command == "rotate right" && !moving) {
+		rotateRight();
 		moving = true;
 		state = command;
 	} else if (command == "up" && !moving) {
@@ -80,59 +106,74 @@ void loop() {
 }
 
 void moveFront() {
-		backLeft.writeMicroseconds(1650);
-		backRight.writeMicroseconds(1650);
-		frontLeft.writeMicroseconds(1650);
-		frontRight.writeMicroseconds(1650);
+		servo2.writeMicroseconds(1650);
+		servo1.writeMicroseconds(1650);
+		servo6.writeMicroseconds(1650);
+		servo5.writeMicroseconds(1650);
 		Serial.println("ROV is moving front");
 }
 
 void moveBack() {
-		backLeft.writeMicroseconds(1350);
-		backRight.writeMicroseconds(1350);
-		frontLeft.writeMicroseconds(1350);
-		frontRight.writeMicroseconds(1350);
+		servo2.writeMicroseconds(1350);
+		servo1.writeMicroseconds(1350);
+		servo6.writeMicroseconds(1350);
+		servo5.writeMicroseconds(1350);
 		Serial.println("ROV is moving back");
 }
 
-void moveLeft() {
-		backLeft.writeMicroseconds(1350);
-		frontLeft.writeMicroseconds(1350);
-		backRight.writeMicroseconds(1650);
-		frontRight.writeMicroseconds(1650);	
-		Serial.println("ROV is moving left");
+void rotateLeft() {
+		servo2.writeMicroseconds(1350);
+		servo6.writeMicroseconds(1350);
+		servo1.writeMicroseconds(1650);
+		servo5.writeMicroseconds(1650);	
+		Serial.println("ROV is rotating to left");
 }
 
-void moveRight() {
-		backLeft.writeMicroseconds(1650);
-		frontLeft.writeMicroseconds(1650);
-		backRight.writeMicroseconds(1350);
-		frontRight.writeMicroseconds(1350);
-
-		Serial.println("ROV is moving right");
+void rotateRight() {
+		servo2.writeMicroseconds(1650);
+		servo6.writeMicroseconds(1650);
+		servo1.writeMicroseconds(1350);
+		servo5.writeMicroseconds(1350);
+		Serial.println("ROV is rotating to right");
 }
 
 void moveUp() {
-		midLeft.writeMicroseconds(1650);
-		midRight.writeMicroseconds(1650);
+		servo4.writeMicroseconds(1650);
+		servo3.writeMicroseconds(1650);
 		Serial.println("ROV is moving up");
 }
 
 void moveDown() {
-		midLeft.writeMicroseconds(1350);
-		midRight.writeMicroseconds(1350);
+		servo4.writeMicroseconds(1350);
+		servo3.writeMicroseconds(1350);
 		Serial.println("ROV is moving down");
+}
+
+void moveLeft() {
+		servo1.writeMicroseconds(1650);
+		servo6.writeMicroseconds(1650);
+		servo2.writeMicroseconds(1350);
+		servo5.writeMicroseconds(1350);
+		Serial.println("ROV is moving left");
+}
+
+void moveRight() {
+		servo1.writeMicroseconds(1650);
+		servo6.writeMicroseconds(1650);
+		servo2.writeMicroseconds(1350);
+		servo5.writeMicroseconds(1350);
+		Serial.println("ROV is moving right");
 }
 
 void stopMoving() {
 	if (state == "front" || state == "back" || state == "left" || state == "right") {
-		backLeft.writeMicroseconds(1500);
-		backRight.writeMicroseconds(1500);		
-		frontLeft.writeMicroseconds(1500);
-		frontRight.writeMicroseconds(1500);
+		servo2.writeMicroseconds(1500);
+		servo1.writeMicroseconds(1500);		
+		servo6.writeMicroseconds(1500);
+		servo5.writeMicroseconds(1500);
 	} else if (state == "up" || state == "down") {
-		midLeft.writeMicroseconds(1500);
-		midRight.writeMicroseconds(1500);
+		servo4.writeMicroseconds(1500);
+		servo3.writeMicroseconds(1500);
 	}
 		Serial.println("ROV HAS STOPPED");
 }
